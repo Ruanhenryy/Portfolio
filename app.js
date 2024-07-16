a40a93662dd7162641b5df4b36584973c151405e
Ruanhenryy <caruaru288@gmail.com>
2024-07-16 14:57:15 -0300

    Responsividade do perfil

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const btnmenu = document.querySelector('#icon')
 const menu = document.querySelector('.conteiner-menu')
 const btnSairMenu = document.querySelector('#icon-menu')
 
+const foto = document.querySelector('.conteiner-foto')
+
 btnmenu.addEventListener('click', function(){
-    menu.classList.add('active')
+    menu.classList.toggle('active')
     btnmenu.style.display = 'none'
     btnSairMenu.style.display = 'block'
+    foto.style.margin = '0'
 })
 
 btnSairMenu.addEventListener('click', function(){
     menu.classList.remove('active')
     btnmenu.style.display = 'block'
     btnSairMenu.style.display = 'none'
+    foto.style.margin = 'auto'
 })
